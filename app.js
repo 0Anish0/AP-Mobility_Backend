64d41b9da3959b9805c35804e7cc771d81142f7b
0Anish0 <anishpandey4576@gmail.com>
2024-07-24 15:28:00 +0530

    update

--- app.js
+++ app.js
@@ -66,9 +66,5 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 module.exports = app;
